f85fc5f0f524238120b1e6603fc6b855789c3062
[sabina-salehova] <[sabinaas@code.edu.az]>
2023-06-08 19:09:13 +0400

    2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMders.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ContentResult Index()
+        //{
+        //    return Content("CONTENT");
+        //}
+
+        //public JsonResult Index()
+        //{
+        //    return Json("JSON RESULT");
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    ViewResult view = new ViewResult
+        //    {
+        //        ViewName="index"
+        //    };
+        //    return view;
+        //}
+
+        //public ViewResult About() 
+        //{
+        //    return new ViewResult { ViewName="About"};
+        //}
+
+        // ------------------------   ViewResult, Json Result, ContentResult her biri ActionResult-dan miras aldigi ucun geri donuw tipini Action Result yaziriq. -----
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
